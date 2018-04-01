ec2ec986b9cc730cfa5568d7e528e542a6f1340b
全建军 <my.candy@outlook.com>
2018-04-01 14:53:44 +0800

    新建糖果脚手架工程

---
+++ src/main/java/com/candy/structure/candystructure/CandyStructureApplication.java
@@ -0,0 +1,13 @@
+package com.candy.structure.candystructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyStructureApplication {
+
+    public static void main(String[] args) {
+        System.out.println("dfsafsdafdsa");
+        SpringApplication.run(CandyStructureApplication.class, args);
+    }
+}
